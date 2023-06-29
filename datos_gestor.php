9cb7e2d12b1576989a9c74e983ba1ca49a10fd95
dieg00s0ri0 <diego9.5.2000jios@gmail.com>
2023-06-29 17:59:17 -0600

    first commit

--- datos_gestor.php
+++ datos_gestor.php
@@ -2,7 +2,7 @@
     <div class=" container col-12">
         <div class="p-3 mx-auto">
             <form action="">
-                <div class="p-2 rounded-4" style=" background-color: #E8ECEF; border: inherit;">
+                <div class="p-2 rounded-4" style=" background-color: #F1F3F3; border: inherit; ">
                     <div class="text-white m-2 align-items-end" style="text-align:right;">
                         <span class="bg-success rounded-2 p-2"><img src="https://img.icons8.com/fluency/30/000000/user-manual.png" />Datos
                             Generales</span>
@@ -10,8 +10,26 @@
                     <div class="row align-items-start form-row">
                         <div class="col-md-3">
                             <div class="md-form form-group">
-                                <label for="clavec" class="form-label">Clave Castastral:*</label>
-                                <input type="text" class="form-control mb-2" id="clavec" name="clavec" readonly value="">
+                                <label for="clavec" class="form-label">Cuenta:*</label>
+                                <input type="text" class="form-control form-control-sm mb-2" name="cuenta" value="" required>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="md-form form-group">
+                                <label for="clavec" class="form-label">Nombre:*</label>
+                                <input type="text" class="form-control form-control-sm mb-2" name="cuenta" value="" required>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="md-form form-group">
+                                <label for="clavec" class="form-label">Telefono Local:*</label>
+                                <input type="text" class="form-control form-control-sm mb-2" name="cuenta" value="" required>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="md-form form-group">
+                                <label for="clavec" class="form-label">Telefono Celular:*</label>
+                                <input type="text" class="form-control form-control-sm mb-2" name="cuenta" value="" required>
                             </div>
                         </div>
                     </div>
